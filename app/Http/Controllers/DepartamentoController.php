e990a7b4fce9efce381ad6eba3488fbb2e7d767c
Carlos Loitti <carlosloitir@gmail.com>
2024-03-29 17:53:34 -0500

    Departamento Delete

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,7 +30,10 @@ class DepartamentoController extends Controller
      */
     public function create()
     {
-        //
+        $paises = DB::table('tb_pais')
+            ->orderBy('pais_nomb')
+            ->get();
+        return view('departamento.new', ['paises' => $paises]);
     }
 
     /**
@@ -40,7 +44,18 @@ class DepartamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $departamento = new Departamento();
+
+        $departamento->depa_nomb = $request->name;
+        $departamento->pais_codi = $request->code;
+        $departamento->save();
+
+        $departamentos = DB::table('tb_departamento')
+        ->join('tb_pais', 'tb_departamento.pais_codi', '=' , 'tb_pais.pais_codi')
+        ->select('tb_departamento.*' , "tb_pais.pais_nomb")
+        ->get();
+
+        return view('departamento.index', ['departamentos' => $departamentos]);
     }
 
     /**
@@ -85,6 +100,14 @@ class DepartamentoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $departamento = Departamento::find($id);
+        $departamento->delete();
+
+        $departamentos = DB::table('tb_departamento')
+        ->join('tb_pais', 'tb_departamento.pais_codi', '=' , 'tb_pais.pais_codi')
+        ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+        ->get();
+
+        return view ('departamento.index' , ['departamentos' => $departamentos]);
     }
 }
