7b982b94fba392a97265900c91d048096b9ad552
Carlos Loitti <carlosloitir@gmail.com>
2024-03-29 17:26:41 -0500

    Municipio List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,6 @@ Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comuna
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy'); 
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');  
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+
+
+Route::get('/municipios' , [MunicipioController::class, 'index'])->name('municipios.index');
